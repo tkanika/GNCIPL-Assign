9ce741a958f3b97cdc75c3ee481144d7142efbd8
Kanika <tomerkanika2002@gmail.com>
2025-09-22 13:02:34 +0530

    added a db name

--- gncipl_assign5/Backend/config/db.js
+++ gncipl_assign5/Backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 //Function to connect to the MongoDb Database
 const connectDB = async () => {
     mongoose.connection.on('connected',()=>console.log('Database connected'));
-    await mongoose.connect(`${process.env.MONGODB_URI}/BudgetTracker`);
+    await mongoose.connect(`${process.env.MONGODB_URI}/ExpenseTracker`);
 }
 
 module.exports = connectDB;
